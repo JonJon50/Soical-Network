dd67a8b4acd2973f588151045d38121aa030660e
John Hagens <ginuwine104@gmail.com>
2023-02-23 15:29:25 -0500

    controllers: thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -71,3 +71,23 @@ addReaction(req, res) {
       res.status(500).json(err);
     });
 };
+
+
+removeReaction(req, res) {
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $pull: { reactions: { reactionId: req.params.reactionId } } },
+    { runValidators: true, new: true }
+  )
+    .then((thought) => {
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+      res.json(thought);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+};
+
