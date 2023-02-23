a4f2bdb7009101b5d943cb2836f2cf1086eff544
John Hagens <ginuwine104@gmail.com>
2023-02-23 15:40:28 -0500

    controllers: fixed code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -52,42 +52,42 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-},
 
-addReaction(req, res) {
-  Thought.findOneAndUpdate(
-    { _id: req.params.thoughtId },
-    { $addToSet: { reactions: req.body } },
-    { runValidators: true, new: true }
-  )
-    .then((thought) => {
-      if (!thought) {
-        return res.status(404).json({ message: 'No thought with this id!' });
-      }
-      res.json(thought);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
 
-
-removeReaction(req, res) {
-  Thought.findOneAndUpdate(
-    { _id: req.params.thoughtId },
-    { $pull: { reactions: { reactionId: req.params.reactionId } } },
-    { runValidators: true, new: true }
-  )
-    .then((thought) => {
-      if (!thought) {
-        return res.status(404).json({ message: 'No thought with this id!' });
-      }
-      res.json(thought);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-};
 
