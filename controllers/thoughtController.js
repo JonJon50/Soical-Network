6c779bac4b22d893ede19e250033d04f28bda0c4
John Hagens <ginuwine104@gmail.com>
2023-02-23 15:22:01 -0500

    controllers: add/reaction/thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -52,4 +52,22 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+},
+
+addReaction(req, res) {
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $addToSet: { reactions: req.body } },
+    { runValidators: true, new: true }
+  )
+    .then((thought) => {
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+      res.json(thought);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 };
